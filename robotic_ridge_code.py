5d2556ab8d7550ecbd1018d0e83d4c691ad26d5a
Jan Chen <jan618chen@gmail.com>
2024-09-23 23:46:16 -0700

    modified:   robotic_ridge_code.py renamed:    data/x_test.p -> x_test.p renamed:    data/x_train.p -> x_train.p renamed:    data/y_test.p -> y_test.p renamed:    data/y_train.p -> y_train.p

--- robotic_ridge_code.py
+++ robotic_ridge_code.py
@@ -19,8 +19,12 @@ def visualize_data(images: np.ndarray, controls: np.ndarray) -> None:
     # Current images are in float32 format with values between 0.0 and 255.0
     # Just for the purposes of visualization, convert images to uint8
     images = images.astype(np.uint8)
-    
-    # TODO: Your code here!
+
+    # visualize 0th, 10th, and 20th images with their corresponding control vector
+    for i in [0, 10, 20]:
+        img = Image.fromarray(images[i])
+        img.show()
+        print("Control Vector for " + str(i) + "-th image:" , controls[i])
 
     
 
@@ -35,7 +39,16 @@ def compute_data_matrix(images: np.ndarray, controls: np.ndarray, standardize: b
         X (ndarray): input array of size (n, 2700) where each row is the flattened image images[i]
         Y (ndarray): label array of size (n, 3) where row i corresponds to the control for X[i]
     """
-    # TODO: Your code here!
+    # flatten x to n, 2700 array
+    n = images.shape[0]
+    images_reshaped = images.reshape(n, -1)
+
+    if standardize:
+        images_reshaped *= 2/255.0
+        images_reshaped -= 1
+
+    return images_reshaped, controls
+
 
 def ridge_regression(X: np.ndarray, Y: np.ndarray, lmbda: float) -> np.ndarray:
     """
@@ -47,7 +60,12 @@ def ridge_regression(X: np.ndarray, Y: np.ndarray, lmbda: float) -> np.ndarray:
     Returns:
         pi (ndarray): learned policy of size (2700, 3)
     """
-    # TODO: Your code here!
+    # Compute pi = (X^T * X + lambda * I)^-1 * X^T * Y
+    D = X.shape[1]
+    pi = np.dot(np.dot(LA.inv(np.dot(X.T, X) + lmbda * np.eye(D)), X.T), Y)
+
+    return pi
+
 
 def ordinary_least_squares(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
     """
@@ -58,7 +76,13 @@ def ordinary_least_squares(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
     Returns:
         pi (ndarray): learned policy of size (2700, 3)
     """ 
-    # TODO: Your code here!
+
+    # Compute pi = (X^T * X)^-1 * X^T * Y
+    pi = np.dot(np.dot(LA.inv(np.dot(X.T, X)), X.T), Y)
+
+    return pi
+
+    
 
 def measure_error(X: np.ndarray, Y: np.ndarray, pi: np.ndarray) -> float:
     """
@@ -70,7 +94,20 @@ def measure_error(X: np.ndarray, Y: np.ndarray, pi: np.ndarray) -> float:
     Returns:
         error (float): the mean Euclidean distance error across all n samples
     """
-    # TODO: Your code here!
+    # Compute average squared Euclidian distance
+
+    n = X.shape[0]
+    avg_Euclidian = 0
+    for i in range(n):
+        avg_Euclidian += LA.norm(np.dot(X[i], pi) - Y[i])**2
+    avg_Euclidian /= n
+
+    return avg_Euclidian
+
+   
+
+    
+
 
 def compute_condition_number(X: np.ndarray, lmbda: float) -> float:
     """
@@ -81,7 +118,14 @@ def compute_condition_number(X: np.ndarray, lmbda: float) -> float:
     Returns:
         kappa (float): condition number of the input array with the given lambda
     """
-    # TODO: Your code here!
+    # Compute condition number of X^T * X + lambda * I
+
+
+    D = X.shape[1]
+
+    condition_number = LA.cond(np.dot(X.T, X) + lmbda * np.eye(D))
+
+    return condition_number
 
 if __name__ == '__main__':
 
@@ -91,4 +135,56 @@ if __name__ == '__main__':
     LAMBDA = [0.1, 1.0, 10.0, 100.0, 1000.0]
 
     # TODO: Your code here!
+    # visualize 0th, 10th, and 20th images with their corresponding controls
+
+    # a) Visualize data
+    # visualize_data(x_train, y_train)
+
+    # b) OLS
+    # X, Y = compute_data_matrix(x_train, y_train)
+    # ordinary_least_squares(X, Y)
+
+    # c) Ridge regression
+
+    """
+        X, Y = compute_data_matrix(x_train, y_train)
+        for i in [0.1, 1.0, 10.0, 100.0, 1000.0]:
+            pi = ridge_regression(X, Y, i)
+            error = measure_error(X, Y, pi)
+            print("Ridge regression with lambda = ", i, " has error = ", error) 
+    """
+    
+
+    # d) standardize and repeat c)
+    """ X, Y = compute_data_matrix(x_train, y_train, standardize=True)
+        for i in [0.1, 1.0, 10.0, 100.0, 1000.0]:
+        pi = ridge_regression(X, Y, i)
+        error = measure_error(X, Y, pi)
+        print("Standardized ridge regression with lambda = ", i, " has error = ", error)  
+     """
+
+    # e) evaluate with and without standardization on test data
+    """ X, Y = compute_data_matrix(x_train, y_train)
+    X_test, Y_test = compute_data_matrix(x_test, y_test)
+    for i in [0.1, 1.0, 10.0, 100.0, 1000.0]:
+        pi = ridge_regression(X, Y, i)
+        error = measure_error(X_test, Y_test, pi)
+        print("Ridge regression (without standardization), lambda = ", i, " has error = ", error)
+    
+    X, Y = compute_data_matrix(x_train, y_train, standardize=True)
+    X_test, Y_test = compute_data_matrix(x_test, y_test, standardize=True)
+    for i in [0.1, 1.0, 10.0, 100.0, 1000.0]:
+        pi = ridge_regression(X, Y, i)
+        error = measure_error(X_test, Y_test, pi)
+        print("Ridge regression (with standardization), lambda = ", i, " has error = ", error)
+
+     """
+
+    # f) condition number for lmbda = 100 with and without standardization
+    X, Y = compute_data_matrix(x_train, y_train)
+    print("Condition number for lambda = 100: ", compute_condition_number(X, 100))
+
+    X, Y = compute_data_matrix(x_train, y_train, standardize=True)
+    print("Condition number for lambda = 100 (standardized): ", compute_condition_number(X, 100))
+
     
